834fe6a7d05186bcb982e99c07a1bebf120bea88
Ítalo Oliveira <italocaua19506@gmail.com>
2024-08-24 22:04:54 -0300

    Remove SQLite files after migration to MySQL

--- setupCRUD/settings.py
+++ setupCRUD/settings.py
@@ -69,8 +69,12 @@ WSGI_APPLICATION = 'setupCRUD.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
